5872d573d1bce77999f36081492a3cd37bf7ec60
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2023-12-05 17:13:46 +0700

    `feat` provide swagger in `main.go`

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/fathoor/simkes-api/core/config"
 	"github.com/fathoor/simkes-api/core/exception"
+	"github.com/gofiber/contrib/swagger"
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -10,7 +11,10 @@ func main() {
 	var (
 		cfg = config.ProvideConfig()
 		app = config.ProvideApp(cfg)
+		api = swagger.New(*config.ProvideSwagger())
 	)
+
+	app.Use(api)
 
 	err := app.Listen(cfg.Get("APP_ADDRESS"))
 	exception.PanicIfError(err)
