c7e07037b935c7d826412998735b0314abcc1576
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2023-12-05 22:14:18 +0700

    `feat` route provider in `main.go`

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/fathoor/simkes-api/core/config"
 	"github.com/fathoor/simkes-api/core/exception"
+	"github.com/fathoor/simkes-api/core/provider"
 	"github.com/gofiber/contrib/swagger"
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -11,9 +12,11 @@ func main() {
 	var (
 		cfg = config.ProvideConfig()
 		app = config.ProvideApp(cfg)
+		db  = config.ProvideDB(cfg)
 		api = swagger.New(*config.ProvideSwagger())
 	)
 
+	provider.ProvideModule(app, db)
 	app.Use(api)
 
 	err := app.Listen(cfg.Get("APP_ADDRESS"))
