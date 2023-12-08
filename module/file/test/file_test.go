aaaab617034d3ecf27f823fb99ff9a742a25659e
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2023-12-08 08:18:35 +0700

    `feat` file test cases

--- module/file/test/file_test.go
+++ module/file/test/file_test.go
@@ -31,48 +31,123 @@ var app = ProvideTestApp()
 var fileTest []byte
 
 func TestFile_Upload(t *testing.T) {
-	body := new(bytes.Buffer)
-	writer := multipart.NewWriter(body)
+	t.Run("When file and type is valid", func(t *testing.T) {
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
 
-	file, err := writer.CreateFormFile("file", "default.png")
-	assert.Nil(t, err)
-	_, err = file.Write(fileTest)
-	assert.Nil(t, err)
+		file, err := writer.CreateFormFile("file", "default.png")
+		assert.Nil(t, err)
+		_, err = file.Write(fileTest)
+		assert.Nil(t, err)
 
-	err = writer.WriteField("type", "image")
-	assert.Nil(t, err)
+		err = writer.WriteField("type", "image")
+		assert.Nil(t, err)
 
-	err = writer.Close()
-	assert.Nil(t, err)
+		err = writer.Close()
+		assert.Nil(t, err)
 
-	request := httptest.NewRequest(http.MethodPost, "/api/v1/file", body)
-	request.Header.Set("Content-Type", writer.FormDataContentType())
+		request := httptest.NewRequest(http.MethodPost, "/api/v1/file", body)
+		request.Header.Set("Content-Type", writer.FormDataContentType())
 
-	response, err := app.Test(request)
-	assert.Nil(t, err)
-	assert.Equal(t, fiber.StatusOK, response.StatusCode)
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusOK, response.StatusCode)
+	})
+
+	t.Run("When file is invalid", func(t *testing.T) {
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+
+		file, err := writer.CreateFormFile("file", "default.bmp")
+		assert.Nil(t, err)
+		_, err = file.Write(fileTest)
+		assert.Nil(t, err)
+
+		err = writer.Close()
+		assert.Nil(t, err)
+
+		request := httptest.NewRequest(http.MethodPost, "/api/v1/file", body)
+		request.Header.Set("Content-Type", writer.FormDataContentType())
+
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, response.StatusCode)
+	})
+
+	t.Run("When type is invalid", func(t *testing.T) {
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+
+		file, err := writer.CreateFormFile("file", "default.png")
+		assert.Nil(t, err)
+		_, err = file.Write(fileTest)
+		assert.Nil(t, err)
+
+		err = writer.WriteField("type", "video")
+		assert.Nil(t, err)
+
+		err = writer.Close()
+		assert.Nil(t, err)
+
+		request := httptest.NewRequest(http.MethodPost, "/api/v1/file", body)
+		request.Header.Set("Content-Type", writer.FormDataContentType())
+
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, response.StatusCode)
+	})
 }
 
 func TestFile_Download(t *testing.T) {
-	request := httptest.NewRequest(http.MethodGet, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.png/download", nil)
+	t.Run("When file exist", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.png/download", nil)
 
-	response, err := app.Test(request)
-	assert.Nil(t, err)
-	assert.Equal(t, fiber.StatusOK, response.StatusCode)
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusOK, response.StatusCode)
+	})
+
+	t.Run("When file not exist", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.bmp/download", nil)
+
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusNotFound, response.StatusCode)
+	})
 }
 
 func TestFile_Get(t *testing.T) {
-	request := httptest.NewRequest(http.MethodGet, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.png", nil)
+	t.Run("When file exist", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.png", nil)
 
-	response, err := app.Test(request)
-	assert.Nil(t, err)
-	assert.Equal(t, fiber.StatusOK, response.StatusCode)
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusOK, response.StatusCode)
+	})
+
+	t.Run("When file not exist", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.bmp", nil)
+
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusNotFound, response.StatusCode)
+	})
 }
 
 func TestFile_Delete(t *testing.T) {
-	request := httptest.NewRequest(http.MethodDelete, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.png", nil)
+	t.Run("When file exist", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodDelete, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.png", nil)
 
-	response, err := app.Test(request)
-	assert.Nil(t, err)
-	assert.Equal(t, fiber.StatusNoContent, response.StatusCode)
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusNoContent, response.StatusCode)
+	})
+
+	t.Run("When file not exist", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodDelete, "/api/v1/file/image/4d32b23d-0927-457d-ab12-25bd632224b1.bmp", nil)
+
+		response, err := app.Test(request)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusNotFound, response.StatusCode)
+	})
 }
