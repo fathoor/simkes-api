e94a8393e67978fcaf4ca7a366bd3d723051ad18
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-03-14 15:29:28 +0700

    chore(exception): refactor handler

--- internal/exception/handler.go
+++ internal/exception/handler.go
@@ -2,52 +2,52 @@ package exception
 
 import (
 	"errors"
-	web "github.com/fathoor/simkes-api/internal/model"
+	"github.com/fathoor/simkes-api/internal/model"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Handler(c *fiber.Ctx, e error) error {
 	var (
-		badRequestError     BadRequestError
-		unauthorizedError   UnauthorizedError
-		forbiddenError      ForbiddenError
-		notFoundError       NotFoundError
-		internalServerError InternalServerError
+		badRequestError     *BadRequestError
+		unauthorizedError   *UnauthorizedError
+		forbiddenError      *ForbiddenError
+		notFoundError       *NotFoundError
+		internalServerError *InternalServerError
 	)
 
 	switch {
 	case errors.As(e, &badRequestError):
-		return c.Status(fiber.StatusBadRequest).JSON(web.Response{
-			Code:   fiber.StatusBadRequest,
-			Status: "Bad Request",
+		return c.Status(badRequestError.Code()).JSON(model.WebResponse{
+			Code:   badRequestError.Code(),
+			Status: badRequestError.Status(),
 			Data:   badRequestError.Error(),
 		})
 	case errors.As(e, &unauthorizedError):
-		return c.Status(fiber.StatusUnauthorized).JSON(web.Response{
-			Code:   fiber.StatusUnauthorized,
-			Status: "Unauthorized",
+		return c.Status(unauthorizedError.Code()).JSON(model.WebResponse{
+			Code:   unauthorizedError.Code(),
+			Status: unauthorizedError.Status(),
 			Data:   unauthorizedError.Error(),
 		})
 	case errors.As(e, &forbiddenError):
-		return c.Status(fiber.StatusForbidden).JSON(web.Response{
-			Code:   fiber.StatusForbidden,
-			Status: "Forbidden",
+		return c.Status(forbiddenError.Code()).JSON(model.WebResponse{
+			Code:   forbiddenError.Code(),
+			Status: forbiddenError.Status(),
 			Data:   forbiddenError.Error(),
 		})
 	case errors.As(e, &notFoundError):
-		return c.Status(fiber.StatusNotFound).JSON(web.Response{
-			Code:   fiber.StatusNotFound,
-			Status: "Not Found",
+		return c.Status(notFoundError.Code()).JSON(model.WebResponse{
+			Code:   notFoundError.Code(),
+			Status: notFoundError.Status(),
 			Data:   notFoundError.Error(),
 		})
 	case errors.As(e, &internalServerError):
-		return c.Status(fiber.StatusInternalServerError).JSON(web.Response{
-			Code:   fiber.StatusInternalServerError,
-			Status: "Internal Server Error",
+		return c.Status(internalServerError.Code()).JSON(model.WebResponse{
+			Code:   internalServerError.Code(),
+			Status: internalServerError.Status(),
 			Data:   internalServerError.Error(),
 		})
 	default:
-		return c.Status(fiber.StatusInternalServerError).JSON(web.Response{
+		return c.Status(fiber.StatusInternalServerError).JSON(model.WebResponse{
 			Code:   fiber.StatusInternalServerError,
 			Status: "Internal Server Error",
 			Data:   e.Error(),
--- internal/model/response.go
+++ internal/model/response.go
@@ -1,6 +1,6 @@
 package model
 
-type Response struct {
+type WebResponse struct {
 	Code   int         `json:"code"`
 	Status string      `json:"status"`
 	Data   interface{} `json:"data"`
