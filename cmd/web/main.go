bbcab3bca27c1cf7c7ca2dbd852da33b15ceee95
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-03-16 15:41:36 +0700

    chore(cmd): move main into web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/fathoor/simkes-api/internal/app/config"
 	"github.com/fathoor/simkes-api/internal/app/exception"
 	"github.com/fathoor/simkes-api/internal/app/provider"
-	"github.com/gofiber/contrib/swagger"
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -15,11 +14,9 @@ func main() {
 		app = config.ProvideApp(cfg)
 		db  = config.ProvideDB(cfg)
 		di  = provider.Provider{App: app, DB: db}
-		api = swagger.New(*config.ProvideSwagger())
 	)
 
 	di.Provide()
-	app.Use(api)
 
 	err := app.Listen(fmt.Sprintf(":%d", cfg.GetInt("APP_PORT")))
 	exception.PanicIfError(err)
