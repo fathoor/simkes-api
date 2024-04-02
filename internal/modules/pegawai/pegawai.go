82ae50e43a6ebd92a7443bde4a771f420d709087
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-03 06:55:15 +0700

    feat(pegawai): rewrite to `sqlx`

--- internal/modules/pegawai/pegawai.go
+++ internal/modules/pegawai/pegawai.go
@@ -7,10 +7,10 @@ import (
 	"github.com/fathoor/simkes-api/internal/modules/pegawai/internal/router"
 	"github.com/fathoor/simkes-api/internal/modules/pegawai/internal/usecase"
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
+	"github.com/jmoiron/sqlx"
 )
 
-func ProvidePegawai(app *fiber.App, db *gorm.DB, validator *config.Validator) {
+func ProvidePegawai(app *fiber.App, db *sqlx.DB, validator *config.Validator) {
 	pegawaiRepository := postgres.NewPegawaiRepository(db)
 	pegawaiUseCase := usecase.NewPegawaiUseCase(&pegawaiRepository)
 	pegawaiController := controller.NewPegawaiController(pegawaiUseCase, validator)
